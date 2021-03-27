664f8681da4743f58b87b8c18e821872c23da154
Helder Avila <helderavila2016@gmail.com>
2021-03-27 09:48:22 -0300

    refactor: Componente sidebar e content

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,32 @@
-export function SideBar() {
+import { GenreResponseProps } from '../App'
+
+// Components
+import { Button } from './Button';
+
+interface SideBarProps {
+  genres: GenreResponseProps[];
+  onClick: (selectedId: number) => void;
+  selectedGenreId: number;
+}
+
+
+export function SideBar({ genres, onClick, selectedGenreId }: SideBarProps) {
   // Complete aqui
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onClick(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
